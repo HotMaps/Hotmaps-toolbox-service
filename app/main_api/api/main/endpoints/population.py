5ba5c18897f28b88a724b91dee9863ac58c58aa8
hud <authentication.dh@gmail.com>
2017-08-25 09:48:44 +0200

    New resource: Population Density by NUTS
    - previous /population/denstiy/area has been deprecated
    - new path is /population/density/nuts/area to replace it
    - new resource returns FeatureCollection

--- app/main_api/api/main/endpoints/population.py
+++ app/main_api/api/main/endpoints/population.py
@@ -11,6 +11,10 @@ from sqlalchemy import func, BigInteger, TypeDecorator
 from main_api.models import db
 import datetime
 import shapely.geometry as shapely_geom
+from geojson import FeatureCollection, Feature, dumps
+from geoalchemy2.shape import to_shape
+
+
 
 log = logging.getLogger(__name__)
 
@@ -68,6 +72,7 @@ class PopDensityInArea(Resource):
 
 @ns.route('/density/area/')
 @api.response(404, 'Density not found for that specific area.')
+@ns.deprecated
 class PopDensityInArea(Resource):
 
     @api.marshal_with(total_density_for_nuts_area)
@@ -82,7 +87,7 @@ class PopDensityInArea(Resource):
         points = api.payload['points']
         poly = shapely_geom.Polygon([[p['lng'], p['lat']] for p in points])
         geom = "SRID=4326;{}".format(poly.wkt)
-        density = db.session.query(func.sum(PopulationDensity.value, type_=CoerceToInt), \
+        query = db.session.query(func.sum(PopulationDensity.value, type_=CoerceToInt), \
                                    func.ST_AsGeoJSON(func.ST_Collect(Nuts.geom))). \
             join(Nuts, PopulationDensity.nuts). \
             filter(PopulationDensity.date == datetime.datetime.strptime(str(year), '%Y')). \
@@ -103,7 +108,76 @@ class PopDensityInArea(Resource):
         output = {
             'year': year,
             'nuts_level': nuts_level,
-            'sum_density': density[0],
-            'geometries': density[1]
+            'sum_density': query[0],
+            'geometries': query[1]
         }
         return output
+
+
+@ns.route('/density/nuts/area/')
+@api.response(404, 'Density not found for that specific area.')
+class PopDensityNutsInArea(Resource):
+
+    #@api.marshal_with(total_density_for_nuts_area)
+    @api.expect(total_density_for_nuts_area_input)
+    def post(self):
+        """
+        Returns the total density for specific area and year
+        :return:
+        """
+        year = api.payload['year']
+        nuts_level = api.payload['nuts_level']
+        points = api.payload['points']
+        poly = shapely_geom.Polygon([[p['lng'], p['lat']] for p in points])
+        geom = "SRID=4326;{}".format(poly.wkt)
+        query = db.session.query(PopulationDensity, \
+                                 func.ST_Area(func.ST_Transform(Nuts.geom, 3035))). \
+            join(Nuts, PopulationDensity.nuts). \
+            filter(PopulationDensity.date == datetime.datetime.strptime(str(year), '%Y')). \
+            filter(Nuts.stat_levl_ == nuts_level). \
+            filter(func.ST_Within(Nuts.geom, func.ST_Transform(func.ST_GeomFromEWKT(geom), 4258))).all()
+
+        output = {
+            'year': year,
+            'nuts_level': nuts_level,
+            'sum_density': query[0],
+            'geometries': query[1]
+        }
+        area = 0.0
+        density = 0
+        features = []
+        for row in query:
+            pop_dens = row[0]
+            nuts = pop_dens.nuts
+
+            # sum area + density
+            density += pop_dens.value
+            area += row[1]
+
+            # create feature
+            geometry = to_shape(nuts.geom)
+            feature = Feature(
+                id=nuts.gid,
+                geometry=geometry,
+                properties={
+                    'density': float(pop_dens.value),
+                }
+            )
+            features.append(feature)
+
+        properties = {
+            'year': year,
+            'nuts_level': nuts_level,
+            'sum_density': density,
+            'area': area
+        }
+
+        crs = {
+            "type": "name",
+            "properties": {
+                "name": "EPSG:%d" % Nuts.CRS
+            }
+        }
+
+        return FeatureCollection(features, properties=properties, crs=crs)
+
