b6c68114f4407b1b53b90762f8536f6b66244419
lesly houndole <lesly.houndole@crem.ch>
2018-04-10 18:54:15 +0200

    commitmessage

--- push-develop.sh
+++ push-develop.sh
@@ -5,4 +5,4 @@ echo Hello, Can you please add the commit message?
 read commitmessage
 git commit -m commitmessage
 git push origin develop
-ssh root@vlhhotmapsdev.ch 'bash -s' < /var/hotmaps/scripts/toolbox-service/update.sh
+ssh crem@vlhhotmapsdev.ch 'bash -s' < /var/hotmaps/scripts/toolbox-service/update.sh
