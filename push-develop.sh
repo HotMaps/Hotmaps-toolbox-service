75cbd7ee6eb31f207ef5669e9dbf3b8a6e52adfe
lesly houndole <lesly.houndole@crem.ch>
2018-04-10 18:56:17 +0200

    commitmessage

--- push-develop.sh
+++ push-develop.sh
@@ -5,4 +5,8 @@ echo Hello, Can you please add the commit message?
 read commitmessage
 git commit -m commitmessage
 git push origin develop
-ssh crem@vlhhotmapsdev.ch 'bash -s' < /var/hotmaps/scripts/toolbox-service/update.sh
+ssh crem@vlhhotmapsdev.ch <<'ENDSSH'
+#commands to run on remote host
+
+sh /var/hotmaps/scripts/toolbox-service/update.sh
+ENDSSH
