6133f021ad05519987b61434a15afb077384c209
albaindufils <albain.dufils@crem.ch>
2019-01-09 10:40:51 +0100

    Description of tests

--- api/tests/routes/indicators/test_indicators.py
+++ api/tests/routes/indicators/test_indicators.py
@@ -7,7 +7,7 @@ from app.model import query_geographic_database
 from app.models.indicators import layersData
 from app.sql_queries import get_exists_table_query
 from .payloads import nuts3_stat
-from flask import request
+
 class TestIndicators(unittest.TestCase):
     
     @classmethod
@@ -25,19 +25,74 @@ class TestIndicators(unittest.TestCase):
     
     
     def test_indicators_nutslau(self):
+        """
+            These tests call the API and try to get result from a specific payload and endpoint. 
+            These tests control the functionality of the indicator, as on the client part.
+            It create the payload, and send it to the API ('api/stats/layers/nuts-lau') from each layer.
+        """
         for layer in layersData:
             nuts3_stat['layers'] = [layer]
             print(nuts3_stat)
-
-            rv, json = self.client.post('http://0.0.0.0/api/stats/layers/nuts-lau', data=nuts3_stat)
+            
             with self.subTest(layer=layer, payload=nuts3_stat):
-                self.assertTrue(rv.status_code == 200)
+                try:
+                    rv, json = self.client.post('api/stats/layers/nuts-lau', data=nuts3_stat)
+                    self.assertTrue(rv.status_code == 200)
+                except Exception as e:
+                    print(e)
+                    self.assertTrue(False)
+    
+    def test_aggregateddata_statlvl_nutslau(self):
+        """
+        This function test if aggregated data are in the database. 
+        The test runs over all indicators and check data in with schema_scalelvl (the schema) and the tablename (tablename of the dataset). 
+        For the tablename, "_lau" and "_nuts" are added are added at the end of the tablename. 
 
+        At nuts level, every stat level are tested (column name: stat_levl_), from 0 to 3.
+            0=NUTS 0
+            1=NUTS 1
+            2=NUTS 2
+            3=NUTS 3
+        The query return the count of rows available in the table. 
+        Result:
+            Query returns 0 => No data avalaible for the specific scale
+            Query return >= 1 => Data are available in the specific scale
+        """
+        for layer in layersData:
+            sql_querie = "select count(*) from "+layersData[layer]['schema_scalelvl']+"."+layersData[layer]['tablename']
+            sql_querie_lau = sql_querie + "_lau limit 100"
+            print(sql_querie_lau)
+            with self.subTest(stat_levl_='lau',tablename=layersData[layer]['schema_scalelvl']+"."+layersData[layer]['tablename']+"_lau"):
+                try:
+                    query = query_geographic_database(sql_querie_lau).fetchone()
+                    self.assertTrue(int(query[0]) >= 1)
+                except Exception as e:
+                    print(e)
+                    self.assertTrue(False)
+            for i in range(0,4):
+                if layersData[layer]['data_aggregated'] == True:
+                    sql_querie_nuts = sql_querie + "_nuts where stat_levl_ = "+str(i)
+                    print(sql_querie_nuts)
+                    with self.subTest(stat_levl_=i,tablename=layersData[layer]['schema_scalelvl']+"."+layersData[layer]['tablename']+"_nuts"):
+                        try:
+                            query = query_geographic_database(sql_querie_nuts).fetchone()
+                            self.assertTrue(int(query[0]) >= 1)
+                        except Exception as e:
+                            print(e)
+                            self.assertTrue(False)
+                    
+            
 
     def test_tablenameschema_exists(self):
+        """
+        This test controls if the table exist in the specified schema.
+        The query returns True or False.
+            True=Tablename exists in the schema
+            False=Tablename does'nt exist in the schema
+        """
         for layer in layersData:
             """
-            Subtest only available on python 3.4
+                Subtest only available on python 3.4
             """
 
             with self.subTest(tablename=layersData[layer]['tablename'],schema_hectare=layersData[layer]['schema_hectare']):
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-"""
-The Application tests collection
-================================
-"""
-BASE_URL = "http://153.109.130.41:5000/api"
