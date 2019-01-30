435c85da285ee420bfc82fe114ddfbd74b88ac1e
Unknown <lucienzuber@gmail.com>
2019-01-30 13:17:34 +0100

    Remove snapshot feature

--- api/app/api_v1/__init__.py
+++ api/app/api_v1/__init__.py
@@ -6,7 +6,6 @@ from ..decorators import etag
 from .computation_module import nsCM
 from .users import nsUsers
 from .upload import nsUpload
-from .snapshot import nsSnapshot
 
 @api.before_request
 def before_request():
--- pytest_suit/routes/snapshot/__init__.py
+++
@@ -1,4 +0,0 @@
-from .. import BASE_URL
-from ..user import test_token
-
-test_config = 'This is my test save'
