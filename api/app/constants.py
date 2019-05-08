e5adca87c377c5fcc1ba2ee0ca276675b2e4c6bf
albaindufils <albain.dufils@crem.ch>
2019-05-08 15:21:43 +0200

    Added file to run

--- api/app/constants.py
+++ api/app/constants.py
@@ -25,9 +25,9 @@ CM_REGISTER_Q = 'rpc_queue_CM_register'
 
 #TODO ********************setup this URL depending on which version you are running***************************
 
-CELERY_BROKER_URL = CELERY_BROKER_URL_DOCKER
+CELERY_BROKER_URL = CELERY_BROKER_URL_LOCAL
 CLIENT_URL = CLIENT_URL_LOCAL
-PORT = PORT_DOCKER
+PORT = PORT_LOCAL
 
 
 #TODO *******************************************************************************
