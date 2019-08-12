62b8958c60fef49cea5c114c03b6098d30d62ec8
Sarah Peris <sarah.peris@crem.ch>
2019-08-12 17:19:19 +0200

    added new route for the export of cm layers

--- pytest_suit/routes/uploads/__init__.py
+++ pytest_suit/routes/uploads/__init__.py
@@ -1,4 +1,6 @@
 from .. import BASE_URL, test_csv_file
 from ..user import test_token
+import uuid
 
 test_upload_name = 'pytest_upload_csv'
+test_uuid = str(uuid.uuid4())
---
+++ pytest_suit/routes/uploads/test_exportCMLayer.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+import requests
+import os
+
+from . import BASE_URL, test_uuid
+
+url = BASE_URL + "/upload/export/cmLayer"
+
+
+class TestExportCMLayer(TestCase):
+    def test_post(self):
+        """
+        this test will pass the upload/export/csv/hectare method
+        """
+        cmd = 'touch /var/tmp/' + test_uuid + '.csv'
+        os.system(cmd)
+
+        payload = {
+            "uuid": test_uuid,
+            "type": "csv",
+        }
+
+        expected_status = 200
+        output = requests.post(url, json=payload)
+        assert output.status_code == expected_status
+
+        cmd = 'rm /var/tmp/' + test_uuid + '.csv'
+        os.system(cmd)
+
+
+    def test_port_wrong_parameters(self):
+        """
+        this test will fail because the wrong parameters are given
+        """
+        payload = {
+            "uuuuuiiiid": test_uuid,
+            "tyefepe": "csv",
+        }
+
+        output = requests.post(url, json=payload)
+
+        expected_status = '531'
+
+        assert output.json()['error']['status'] == expected_status
+
+    def test_post_wrong_layer(self):
+        """
+        this test will fail because the uuid does not exist
+        """
+        payload = {
+            "uuid": "fake_uuid",
+            "type": "csv"
+        }
+
+        output = requests.post(url, json=payload)
+
+        expected_status = '541'
+
+        assert output.json()['error']['status'] == expected_status
