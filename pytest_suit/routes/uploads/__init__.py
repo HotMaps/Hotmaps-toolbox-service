0bd62727fd932aedc3761d3255dec038a82cf651
Sarah Peris <sarah.peris@crem.ch>
2019-08-14 08:15:56 +0200

    uuid random and unique

--- pytest_suit/routes/uploads/__init__.py
+++ pytest_suit/routes/uploads/__init__.py
@@ -1,5 +1,6 @@
 from .. import BASE_URL, test_csv_file, test_tif_file
 from ..user import test_token
+import uuid
 
 test_upload_name = 'pytest_upload_csv'
-test_uuid = 'test'
+test_uuid = str(uuid.uuid4())
