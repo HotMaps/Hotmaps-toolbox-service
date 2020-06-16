65e3d263865110eb527b9e9ef7ac1c44ec93a56b
LucienZ <lucienzuber@gmail.com>
2020-06-16 10:38:00 +0200

    test_time_limit

--- api/app/api_v1/users.py
+++ api/app/api_v1/users.py
@@ -41,13 +41,23 @@ ns = nsUsers
 class AskingPasswordRecovery(Resource):
     @api.marshal_with(user_ask_recovery_output)
     @api.expect(user_ask_recovery_input)
-    @celery.task(name='ask for password recovery')
+    @celery.task(name='ask for password recovery', soft=2, hard=4, reply=True)
     def post(self):
         """
 		Method to ask for a Password recovery
 		:return:
 		"""
         # Entries
+        import time
+        try:
+            time.sleep(5)
+            return {
+                "message": "failure"
+            }
+        except:
+            return{
+                "message": "success"
+            }
         try:
             email = api.payload['email']
         except:
