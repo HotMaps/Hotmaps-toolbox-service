321b2de2857243ae759705fa2bd94386c60011b7
Malik Bougacha <malik.bougacha@optiopay.com>
2020-08-31 15:13:35 +0200

    fix line end

--- api/run_all_local_terminal.sh
+++ api/run_all_local_terminal.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+TERM=gnome-terminal
 
-gnome-terminal -e "python producer_cm_alive.local.py" --title="HTAPI: producer_cm_alive"
+$TERM -e "python producer_cm_alive.local.py" --title="HTAPI: producer_cm_alive"
 
-gnome-terminal -e "python run.local.py" --title="HTAPI: run API"
+$TERM -e "python run.local.py" --title="HTAPI: run API"
 
-gnome-terminal -e "python consumer_cm_register.local.py" --title="HTAPI: consumer_cm_register"
+$TERM -e "python consumer_cm_register.local.py" --title="HTAPI: consumer_cm_register"
 
-gnome-terminal -e "celery -A celery_worker_local.celery worker --loglevel=info" --title="HTAPI: celery3"
+$TERM -e "celery -A celery_worker_local.celery worker --loglevel=info" --title="HTAPI: celery3"
